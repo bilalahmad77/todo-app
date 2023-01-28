b3a9b5b4f8daaaabaf53e1a77bfc60bdfb81a42c
bilalahmad77 <mebilalahmad1@gmail.com>
2023-01-28 16:32:19 +0500

    todo-app
    todo-app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+// import './globals.css'
 
 export default function RootLayout({
   children,
@@ -12,7 +12,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+      <h1>Todo App</h1>
+        {children}
+        </body>
     </html>
   )
 }
+
--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    async rewrites() {
+        return [{
+            source: "/api/:path*",
+            destination: "http://127.0.0.1:3001/api/:path*",
+        }, ];
+    },
+};
 
 module.exports = nextConfig
